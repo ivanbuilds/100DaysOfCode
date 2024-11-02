8b3733e61dc57e0eeb5a1eb9160ee1b11433ebb4
Ivanroman0 <ivanromanok@gmail.com>
2024-11-02 12:13:05 -0300

    Day 2: Practiced data types and variables

---
+++ Day2/DataTypesAndVariables.java
@@ -0,0 +1,22 @@
+package datatypesandvariables;
+
+public class DataTypesAndVariables {
+
+    public static void main(String[] args) {
+    
+        int age = 21;
+        double height = 1.84;
+        boolean hasLicense = true;
+        char licenseGrade = 'B';
+        String fullName = "Franco Colapinto";
+        long licenseNumber = 21658446;
+        
+        System.out.println("Full name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Has license?: " + hasLicense);
+        System.out.println("License grade: " + licenseGrade);
+        System.out.println("License number: " + licenseNumber);
+        
+    }
+}
