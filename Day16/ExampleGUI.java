efbb0f10c5b50e730bf0ae8c5e3d42c3dc2ab266
techwithivan <soyivanroman@gmail.com>
2024-11-16 23:50:34 -0300

    Day 16: Learned about the GUI

---
+++ Day16/ExampleGUI.java
@@ -0,0 +1,16 @@
+package logic;
+
+import gui.mainWindow;
+
+public class ExampleGUI {
+
+    public static void main(String[] args) {
+        
+        mainWindow window = new mainWindow();
+        
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        
+    }
+    
+}
