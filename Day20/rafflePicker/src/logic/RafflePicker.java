678d4cec8d2e7411590e24e837af2aac7a044763
techwithivan <soyivanroman@gmail.com>
2024-11-20 23:56:41 -0300

    Day 20: Completed a raffle picker app using java swing

---
+++ Day20/rafflePicker/src/logic/RafflePicker.java
@@ -0,0 +1,16 @@
+package logic;
+
+import gui.mainFrame;
+
+public class RafflePicker {
+
+    public static void main(String[] args) {
+        
+        mainFrame window = new mainFrame();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
